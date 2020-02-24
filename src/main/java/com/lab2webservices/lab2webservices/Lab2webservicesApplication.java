436e635cf980159f2856cff1943ac7ded10bd4f1
Skoorps <kpOC8&1I3$Js&03*bWN8>
2020-02-24 17:54:16 +0100

    initial commit

---
+++ src/main/java/com/lab2webservices/lab2webservices/Lab2webservicesApplication.java
@@ -0,0 +1,13 @@
+package com.lab2webservices.lab2webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2webservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2webservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab2webservices/lab2webservices/Lab2webservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab2webservices.lab2webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2webservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
