fb5841c4881d7b4e768985668308a39e3c5db3f9
Skoorps <skoorps@gmail.com>
2020-03-05 13:38:53 +0100

    changed to requestmapping

--- src/main/java/com/lab2webservices/lab2webservices/BrandController.java
+++ src/main/java/com/lab2webservices/lab2webservices/BrandController.java
@@ -3,8 +3,8 @@ package com.lab2webservices.lab2webservices;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
+@RequestMapping("/api/v1/brands")
 @RestController
 public class BrandController {
 
@@ -14,22 +14,18 @@ public class BrandController {
         this.repository = repository;
     }
 
-    @GetMapping("/api/brands")
+    @GetMapping
     public List<Brand> all() {
         return repository.findAll();
     }
 
-    @GetMapping(value = "/api/brands/{brandName}")
+    @GetMapping(value = "{brandName}")
     public Brand one(@PathVariable String brandName) {
         return repository.findByBrandName(brandName);
     }
 
-//    @GetMapping(value = "/api/phones/brand/{brandId}")
-//    public Optional<Brand> one(@PathVariable Long brandId) {
-//        return repository.findById((long) brandId);
-//    }
 
-    @PostMapping("/api/brands")
+    @PostMapping
     Brand newBrand(@RequestBody Brand brand) {
         return repository.save(brand);
     }
